d239182bbb71326d2c03eed42182c6dd66093a6d
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-15 13:45:27 -0300

    Add camera snap and win sounds

---
+++ sound-controls.js
@@ -0,0 +1,45 @@
+(() => {
+  const basePlaySound = playSound;
+
+  function tone(frequency, start, duration, volume, type = "sine") {
+    audio ||= new AudioContext();
+    if (audio.state === "suspended") audio.resume();
+    const oscillator = audio.createOscillator();
+    const gain = audio.createGain();
+    oscillator.type = type;
+    oscillator.frequency.setValueAtTime(frequency, start);
+    gain.gain.setValueAtTime(.0001, start);
+    gain.gain.exponentialRampToValueAtTime(volume, start + .012);
+    gain.gain.exponentialRampToValueAtTime(.0001, start + duration);
+    oscillator.connect(gain);
+    gain.connect(audio.destination);
+    oscillator.start(start);
+    oscillator.stop(start + duration + .02);
+  }
+
+  playSound = function(type) {
+    if (type !== "camera" && type !== "win") return basePlaySound(type);
+    audio ||= new AudioContext();
+    const now = audio.currentTime;
+    if (type === "camera") {
+      tone(220, now, .12, .025, "triangle");
+      tone(330, now + .045, .13, .018, "triangle");
+      return;
+    }
+    tone(392, now, .34, .045);
+    tone(523.25, now + .09, .38, .04);
+    tone(659.25, now + .18, .48, .035);
+  };
+
+  const baseSnapCamera = snapCamera;
+  snapCamera = function(targetYaw, targetPitch) {
+    if (targetYaw !== cameraYaw || targetPitch !== cameraPitch) playSound("camera");
+    return baseSnapCamera(targetYaw, targetPitch);
+  };
+
+  const baseCompleteLevel = completeLevel;
+  completeLevel = function() {
+    playSound("win");
+    return baseCompleteLevel();
+  };
+})();
