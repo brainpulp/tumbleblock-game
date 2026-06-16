82211be10cd31567441b412328e9e85c285bca07
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-16 15:58:46 -0300

    Stamp cache-busted orthogonal camera build

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-16T18:55:50Z",
-  commit: "camera-orthogonal-axis-c6b562e",
+  deployedAt: "2026-06-16T18:58:40Z",
+  commit: "cache-busted-orthogonal-ff0869c",
 };
