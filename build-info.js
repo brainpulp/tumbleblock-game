d468b6b6aa74086239a8c69dbc0c0a612cc49799
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-15 22:33:49 -0300

    Stamp visible camera axes build

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-16T01:25:49Z",
-  commit: "camera-snap-path-01dc958",
+  deployedAt: "2026-06-16T01:33:44Z",
+  commit: "camera-axes-ab9c497",
 };
