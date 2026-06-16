a6b1e1a8b70d0ef9ce3f110a0521e74d1e62d292
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-16 16:25:26 -0300

    Make build stamp visible

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-16T19:19:53Z",
-  commit: "camera-axis-truth-20260616-191953",
+  deployedAt: "2026-06-16T19:24:56Z",
+  commit: "stamp-visible-20260616-192456",
 };
