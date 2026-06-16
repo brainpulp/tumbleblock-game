d5e1dc4b1b8517cfe3f5fa76010cb4aca415b8c0
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-16 15:47:45 -0300

    Stamp body diagonal camera build

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-16T18:27:50Z",
-  commit: "camera-vertical-cycle-250c292",
+  deployedAt: "2026-06-16T18:47:39Z",
+  commit: "camera-body-axes-1eafaca",
 };
