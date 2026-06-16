39f4c97a2e92a4ac883a860a96a7e96e7df4f7e6
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-15 22:25:59 -0300

    Stamp camera snap path fix

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-16T01:19:45Z",
-  commit: "six-view-camera-0c77714",
+  deployedAt: "2026-06-16T01:25:49Z",
+  commit: "camera-snap-path-01dc958",
 };
