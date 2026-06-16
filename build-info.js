7d05d54a9e21686fdb0d93d3b33c462dbd336b87
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-16 17:10:38 -0300

    Stamp surface drag roll build

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-16T20:03:34Z",
-  commit: "normal-levels-restored-20260616-200334",
+  deployedAt: "2026-06-16T20:09:21Z",
+  commit: "surface-drag-roll-20260616-200921",
 };
