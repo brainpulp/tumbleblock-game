74575ee5808340558b26c1bab3d2a4982f651ac6
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-17 10:16:06 -0300

    Stamp footer record strip build

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-17T04:21:59Z",
-  commit: "fair-push-roll-20260617-042159",
+  deployedAt: "2026-06-17T13:14:13Z",
+  commit: "footer-record-strip-20260617-131413",
 };
