fee6e2e95eb91d37cf4e4c56486da0c45e40fe0c
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-16 22:58:07 -0300

    Stamp continuous camera debug build

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-16T20:09:21Z",
-  commit: "surface-drag-roll-20260616-200921",
+  deployedAt: "2026-06-17T01:55:46Z",
+  commit: "continuous-camera-debug-level-20260617-015546",
 };
