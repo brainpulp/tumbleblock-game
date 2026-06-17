3fec18da5a2607799e28dbe188bf37ead89f2a2d
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-17 10:48:36 -0300

    Update build stamp for top keyboard help

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-17T14:21:30Z",
-  commit: "keyboard-ghost-preview-20260617-142130",
+  deployedAt: "2026-06-17T14:36:20Z",
+  commit: "top-keyboard-help-20260617-143620",
 };
