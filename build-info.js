1b4cbed5127bc6196585261cdcf99f3d3e7e103a
brainpulp <maxi.goldschwartz@gmail.com>
2026-06-17 10:38:01 -0300

    Update build stamp for keyboard controls

--- build-info.js
+++ build-info.js
@@ -1,4 +1,4 @@
 window.TUMBLEBLOCK_BUILD = {
-  deployedAt: "2026-06-17T13:14:13Z",
-  commit: "footer-record-strip-20260617-131413",
+  deployedAt: "2026-06-17T13:48:20Z",
+  commit: "keyboard-controls-20260617-134820",
 };
